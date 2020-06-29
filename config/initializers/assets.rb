58756d93fcb0312e28bc2bfef11a617db991570e
MAIKON DOUGLAS <maikonconta0@gmail.com>
2020-06-28 23:36:37 -0300

    Portanto o tema Gentella para o User

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,30 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # /app/assets
- Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
+ Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css users_backoffice.css users_backoffice.js)
 # /lib/assets
- Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css )
+ Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css 
+                                                   jquery.mCustomScrollbar.concat.css 
+                                                   jquery.mCustomScrollbar.concat.js 
+                                                   custom.css 
+                                                   custom.js
+                                                   american-express.png
+                                                   cropper.jpg
+                                                   favicon.ico
+                                                   img.jpg
+                                                   inbox.png
+                                                   mastercard.png
+                                                   media.png
+                                                   paypal.png
+                                                   picture.png
+                                                   prod-1.jpg
+                                                   prod-2.jpg
+                                                   prod-3.jpg
+                                                   prod-4.jpg
+                                                   prod-5.jpg
+                                                   user.png
+                                                   visa.png )
+# /vendor/assets
+Rails.application.config.assets.precompile += %w( jquery-2.2.4/dist/jquery.js 
+                                                  bootstrap-4.3.1/dist/js/bootstrap.js 
+                                                  bootstrap-4.3.1/dist/css/bootstrap.css)
