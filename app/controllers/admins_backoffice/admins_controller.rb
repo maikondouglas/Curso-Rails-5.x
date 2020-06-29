c3cf4307fa693bbfd2ff2c23fa07bed89c6c53f6
MAIKON DOUGLAS <maikonconta0@gmail.com>
2020-06-29 19:52:44 -0300

    Cadastrando novos administradores

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -7,6 +7,19 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   end
 
   def edit; end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create 
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Administrador cadastrado com sucesso!"
+    else
+      render :new
+    end
+  end
   
   def update
     if @admin.update(params_admin)
