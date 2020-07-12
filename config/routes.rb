6076f3c3bff5b236349f2d94451a0c74fd7d38ac
MAIKON DOUGLAS <maikonconta0@gmail.com>
2020-07-12 00:47:41 -0300

    Ativando as respostas das perguntas

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
   namespace :site do
-    get 'welcome/index'
-    get 'search', to: 'search#questions'
+    get  'welcome/index'
+    get  'search', to: 'search#questions'
+    post 'answer', to: 'answer#question'
   end
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
